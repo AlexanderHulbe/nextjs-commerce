a9a5fc55061d3b28f1a0428a66ac9915ca01bcd1
Alexander Hulbe <34372315+AlexanderHulbe@users.noreply.github.com>
2023-08-24 14:19:30 +0200

    logo changed and carousel improved commit

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -9,7 +9,7 @@ export async function Carousel() {
   if (!products?.length) return null;
 
   // Purposefully duplicating products to make the carousel loop and not run out of products on wide screens.
-  const carouselProducts = [...products, ...products, ...products];
+  const carouselProducts = [...products, ...products, ...products, ...products];
 
   return (
     <div className=" w-full overflow-x-hidden pb-6 pt-1">
